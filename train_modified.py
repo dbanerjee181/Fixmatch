d7a4dca757ba3a4f1536dfbc15e5041c9f53d529
dbanerjee181 <32031591+dbanerjee181@users.noreply.github.com>
2024-10-31 06:47:02 +0000

    Update train_modified.py

--- train_modified.py
+++ train_modified.py
@@ -79,14 +79,14 @@ def main():
     parser.add_argument('--num-workers', type=int, default=4,
                         help='number of workers')
     parser.add_argument('--dataset', default='cifar10', type=str,
-                        choices=['cifar10', 'cifar100'],
+                        choices=['cifar10', 'cifar100','svhn','mnist','stl10'],
                         help='dataset name')
     parser.add_argument('--num-labeled', type=int, default=4000,
                         help='number of labeled data')
     parser.add_argument("--expand-labels", action="store_true",
                         help="expand labels to fit eval steps")
     parser.add_argument('--arch', default='wideresnet', type=str,
-                        choices=['wideresnet', 'resnext'],
+                        choices=['wideresnet', 'resnext','resnet50'],
                         help='dataset name')
     parser.add_argument('--total-steps', default=2**20, type=int,
                         help='number of total steps to run')
