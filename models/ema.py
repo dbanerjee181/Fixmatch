d712a58258680cf78d4d6b2cebbf8dd6f83c74a7
dbanerjee181 <32031591+dbanerjee181@users.noreply.github.com>
2024-11-10 06:21:50 +0000

    Add files via upload

---
+++ models/ema.py
@@ -0,0 +1,52 @@
+"""
+This module implements Exponential Moving Average (EMA) functionality.
+
+EMA is a type of moving average that gives more weight to recent data points, 
+making it more responsive to changes in the data compared to a simple moving average.
+
+The module provides a class `EMA` that can be used to:
+- Update the EMA with new data points.
+- Calculate the current EMA value.
+- Decay the EMA over time.
+
+"""
+
+
+from copy import deepcopy
+
+import torch
+
+
+class ModelEMA(object):
+    def __init__(self, args, model, decay):
+        self.ema = deepcopy(model)
+        self.ema.to(args.device)
+        self.ema.eval()
+        self.decay = decay
+        self.ema_has_module = hasattr(self.ema, 'module')
+       
+        self.param_keys = [k for k, _ in self.ema.named_parameters()]
+        self.buffer_keys = [k for k, _ in self.ema.named_buffers()]
+        for p in self.ema.parameters():
+            p.requires_grad_(False)
+
+    def update(self, model):
+        needs_module = hasattr(model, 'module') and not self.ema_has_module
+        with torch.no_grad():
+            msd = model.state_dict()
+            esd = self.ema.state_dict()
+            for k in self.param_keys:
+                if needs_module:
+                    j = 'module.' + k
+                else:
+                    j = k
+                model_v = msd[j].detach()
+                ema_v = esd[k]
+                esd[k].copy_(ema_v * self.decay + (1. - self.decay) * model_v)
+
+            for k in self.buffer_keys:
+                if needs_module:
+                    j = 'module.' + k
+                else:
+                    j = k
+                esd[k].copy_(msd[j])
